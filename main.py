91fe7b79bf89369c061764b94c536d78b47f0f2e
Cris-aian Vergara <cris.vergara@achievewithoutborders.com>
2023-07-08 15:21:19 +0800

    First commit.

---
+++ main.py
@@ -0,0 +1,26 @@
+def add_create():
+    """Create Book"""
+    pass
+
+
+def add_update():
+    """Update Book"""
+    pass
+
+
+def add_delete():
+    """Delete Book"""
+    pass
+
+
+book = []
+
+title = input("Book Title: ")
+author = input("Book Author: ")
+isbn = input("Book ISBN: ")
+
+
+
+
+
+
